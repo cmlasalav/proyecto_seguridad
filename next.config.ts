93c6bbc92cf5cded364273400796f046834f15c4
Carlos Lasala Villegas <carlos.lasala.villegas@gmail.com>
2025-08-22 09:03:48 -0600

    feat: configure Next.js build process with environment variable and update artifact path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
-
+const path = require("path");
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+  webpack(config) {
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+    return config;
+  },
 };
 
 export default nextConfig;
